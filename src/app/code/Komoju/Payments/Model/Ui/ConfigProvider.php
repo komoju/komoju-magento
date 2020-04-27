763753d09594a65d28434155a7ec17d9aaebcb7a
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-04-27 11:33:22 +1000

    Add ConfigProvider and link to admin options
    Adding the dependency injection instructions (di.xml) and linking to the
    config with the <model> attribute in config.xml. Also adding the
    ConfigProvider as that will be used to deliver config options to the
    frontend while rendering the checkout form.

---
+++ src/app/code/Komoju/Payments/Model/Ui/ConfigProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Komoju\Payments\Model\Ui;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Braintree\Gateway\Config\Config;
+use Magento\Framework\Session\SessionManagerInterface;
+
+class ConfigProvider implements ConfigProviderInterface {
+    const CODE = 'komoju_payments';
+
+        /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var SessionManagerInterface
+     */
+    private $session;
+
+    /**
+     * Constructor
+     *
+     * @param Config $config
+     * @param SessionManagerInterface $session
+     */
+    public function __construct(
+        Config $config,
+        SessionManagerInterface $session
+    ) {
+        $this->config = $config;
+        $this->session = $session;
+    }
+
+    /**
+     * Retrieve assoc array of checkout configuration
+     *
+     * @return array
+     */
+    public function getConfig() {
+        $storeId = $this->session->getStoreId();
+        $isActive = $this->config->isActive($storeId);
+
+        return [
+            'payment' => [
+                self::CODE => [
+                    'isActive' => $isActive,
+                    'title' => $this->config->getTitle(),
+                    'description' => $this->config->getDescription(),
+                    'enable_credit_card_payments' => $this->config->getEnableCreditCardPayments(),
+                    'enable_konbini_payments' => $this->config->getEnableKonbiniPayments(),
+                    'merchant_id'  => $this->config->getMerchantId(),
+                    'secret_key' => $this->config->getSecretKey(),
+                    'webhook_secret_token' => $this->config->getWebhookSecretToken()
+                ]
+            ]
+        ]
+    }
+
+}
