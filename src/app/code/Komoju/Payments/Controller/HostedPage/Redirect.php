ff0cdb904c4a9c959f25d4e7d8d6ef25ecd20360
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-05-05 14:59:08 +1000

    Add Redirect action to send user to the Hosted Page
    Adding a controller action to create the Hosted Page API url and
    redirect the user to it. This is being navigated to after the customer
    has clicked submit and the order data has been saved by Magento, so
    we're retrieving the Order and Billing details, getting the required
    fields from them and then generating the HostedPage URL. Magento follows
    a similar pattern for matching controllers to routes that Rails does, so
    this file can be navigated to at <site-url>/komoju/hostedpage/redirect.
    
    This is just an initial PoC of getting the redirect to work, so the
    return and cancel URLs still need to be populated with the correct data,
    and the external order num needs to be updated to be more specific to
    Magento.

---
+++ src/app/code/Komoju/Payments/Controller/HostedPage/Redirect.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Komoju\Payments\Controller\HostedPage;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Quote\Api\CartRepositoryInterface;
+
+class Redirect extends \Magento\Framework\App\Action\Action {
+
+    /**
+     * @var \Magento\Framework\Controller\Result\RedirectFactory
+     */
+    protected $_resultRedirectFactory;
+
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var \Komoju\Payments\Gateway\Config\Config
+     */
+    private $config;
+
+    private $order = false;
+
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\Controller\Result\RedirectFactory $resultRedirectFactory,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Komoju\Payments\Gateway\Config\Config $config,
+        \Psr\Log\LoggerInterface $logger = null
+    ) {
+        $this->logger = $logger ?: ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class);
+        $this->_resultRedirectFactory = $resultRedirectFactory;
+        $this->_checkoutSession = $checkoutSession;
+        $this->config = $config;
+
+        return parent::__construct($context);
+    }
+    
+    public function execute() {
+        $orderId = $this->getOrder()->getEntityId();
+        $hostedPageParams = $this->getHostedPageParams();
+        $paymentMethod = $this->getRequest()->getParam('payment_method');
+
+        $hostedPageUrl = $this->createHostedPageUrl();
+
+
+        $this->logger->info('******** hostedPageUrl: ' . $hostedPageUrl);
+        // $this->logger->info('$orderId: ' . $orderId);
+        // $this->logger->info('******** payment method: ' . $paymentMethod );
+        // $this->logger->info('******** merchant ID: ' . $this->config->getMerchantId());
+        // $this->logger->info('******** secret key: ' . $this->config->getSecretKey());
+        // $this->logger->info('******** webhook secret token: ' . $this->config->getWebhookSecretToken());
+        // $this->logger->info('****** hosted page params: ' . implode(' - ', $hostedPageParams));
+
+
+        $resultRedirect = $this->_resultRedirectFactory->create();
+        $resultRedirect->setUrl($hostedPageUrl);
+
+        return $resultRedirect;
+    }
+
+    private function createHostedPageUrl() {
+        $hostedPageParams = $this->getHostedPageParams();
+        $paymentMethod = $this->getRequest()->getParam('payment_method');
+        $merchantId = $this->config->getMerchantId();
+        $secretKey = $this->config->getSecretKey();
+        $komojuEndpoint = '/ja/api/'.$merchantId. '/transactions/';
+
+        $qsParams = array();
+		foreach ($hostedPageParams as $key => $val) {
+			$qsParams[] = urlencode($key) . '=' . urlencode($val);
+		}
+		sort($qsParams);
+		$queryString = implode('&', $qsParams);
+
+		$url = $komojuEndpoint.$paymentMethod.'/new'. '?' .$queryString;
+		$hmac = hash_hmac('sha256', $url, $secretKey);
+		$queryString .= '&hmac='.$hmac;
+
+		return 'https://komoju.com/'.$komojuEndpoint.$paymentMethod.'/new'. '?' . $queryString;
+    }
+
+    private function getOrder() {
+
+        if (!$this->order) {
+            $this->order = $this->_checkoutSession->getLastRealOrder();
+        }
+        return $this->order;
+    }
+
+    private function getHostedPageParams() {
+        $order = $this->getOrder();
+        $billingAddress = $order->getBillingAddress();
+
+        return array(
+            "transaction[amount]" => $order->getGrandTotal(),
+            "transaction[currency]" => $order->getOrderCurrencyCode(),
+            "transaction[customer][email]" => $billingAddress->getEmail(),
+            "transaction[customer][given_name]" => $billingAddress->getFirstname(),
+            "transaction[customer][family_name]" => $billingAddress->getLastname(),
+            "transaction[customer][phone]" => $billingAddress->getTelephone(),
+            "transaction[tax]" => $order->getTaxAmount(),
+            "timestamp" => time(),
+            // "via" => "Magento",
+
+            // TODO make sure these are real values
+            "transaction[return_url]" => "https://komoju.com",
+            "transaction[cancel_url]" => "https://komoju.com",
+            "transaction[external_order_num]" => $order->getEntityId(),
+        );
+    }
+}
