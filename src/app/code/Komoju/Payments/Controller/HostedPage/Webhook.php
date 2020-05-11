878b3d7102d47724fa355ee7e76ad7b95e48fe24
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-05-11 11:38:21 +1000

    Update order status when payment captured
    When the app receives a payment.captured event from Komoju then the
    payment is complete, and the order is marked in the system as complete.
    This is the initial event to verify that the process works, and will be
    expanded to cover additional event types and fleshed out with more
    functionality in later commits.

--- src/app/code/Komoju/Payments/Controller/HostedPage/Webhook.php
+++ src/app/code/Komoju/Payments/Controller/HostedPage/Webhook.php
@@ -11,6 +11,16 @@ use Magento\Framework\App\CsrfAwareActionInterface;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 
+use Komoju\Payments\Model\WebhookEvent;
+
+/**
+ * API endpoint to receive Webhook notifications from Komoju. To make the
+ * endpoint a POST endpoint a Magento Action needs to implement the
+ * HttpPostActionInterface. And because all POST have automatic CSRF checking,
+ * and as this is a API endpoint to receive JSON requests it doesn't have a
+ * CSRF token, this class is also implementing the CsrfAwareActionInterface
+ * interface and disabling checking by returning false from the inherited methods.
+ */
 class Webhook extends \Magento\Framework\App\Action\Action implements HttpPostActionInterface, CsrfAwareActionInterface {
     /**
      * @var \Magento\Framework\Controller\ResultFactory
@@ -62,19 +72,45 @@ class Webhook extends \Magento\Framework\App\Action\Action implements HttpPostAc
 
         $this->logger->info('HMAC Valid, processing event');
 
+        
         // grab body
+        $requestBody = $this->getRequest()->getContent();
         // convert to assoc array
+        $webhookEvent = new WebhookEvent($requestBody);
         // grab type
+        
+        $this->logger->info('event type: ' . $webhookEvent->eventType());
+
+        $orderId = $webhookEvent->externalOrderNum();
+        $order = $this->getOrder($orderId);
         // update order according to the type (mark as complete, authorized, failed, etc)
+
+        if($webhookEvent->eventType() == 'payment.captured') {
+            $order->setState(Order::STATE_COMPLETE);
+            $order->setStatus(Order::STATE_COMPLETE);
+            // $order->addStatusHistoryComment('Order complete!');
+            $order->save();
+        }
         
         $result = $this->_resultFactory->create(ResultFactory::TYPE_JSON);
         $result->setHttpResponseCode(200);
     }
 
+    /**
+     * Required to implement by the CsrfAwareActionInterface. Since all requests
+     * are valid there's no need for any validation exception handling, so
+     * returning null just uses Magento's defaults.
+     */
     public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException {
         return null;
     }
 
+    /**
+     * By default all non-AJAX post requests do CSRF checking. Because this
+     * endpoint is only receiving JSON requests there is no CSRF token, so 
+     * returning true here to mark all requests as CSRF valid. If this is not
+     * set then Magento will fail on all requests from Komoju
+     */
     public function validateForCsrf(RequestInterface $request): ?bool {
         return true;
     }
