f55f52d35e2c40f958f380cbd9f162cc978c8089
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-05-26 14:49:37 +1000

    Add comments for the ConfigProvider class
    Also removed the logger from the constructor as it wasn't being used.

--- src/app/code/Komoju/Payments/Model/Ui/ConfigProvider.php
+++ src/app/code/Komoju/Payments/Model/Ui/ConfigProvider.php
@@ -9,13 +9,23 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 
 use Komoju\Payments\Gateway\Config\Config;
 
+/**
+ * The ConfigProvider class is responsible for passing config variables to the
+ * checkout page, to be used by the JS that gets run when the customer decides to
+ * purchase with Komoju.
+ */
 class ConfigProvider implements ConfigProviderInterface {
+    /**
+     * This code is the constant used to ensure that the module is only
+     * operating on itself, and not accidentally changing values in any other
+     * plugin. It's passed around via the di.xml files.
+     */
     const CODE = 'komoju_payments';
     
     // when this array updates make sure to update the comment at system.xml:14
     const ALLOWABLE_CURRENCY_CODES = ['JPY'];
 
-        /**
+    /**
      * @var Config
      */
     private $config;
@@ -26,36 +36,33 @@ class ConfigProvider implements ConfigProviderInterface {
     private $session;
 
     /**
-     * @var Psr\Log\LoggerInterface
-     */
-    private $logger;
-
-    /**
      * Constructor
      *
      * @param Config $config
      * @param SessionManagerInterface $session
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         Config $config,
         SessionManagerInterface $session,
-        ScopeConfigInterface $scopeConfig,
-        \Psr\Log\LoggerInterface $logger = null
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->config = $config;
         $this->session = $session;
         $this->scopeConfig = $scopeConfig;
-        $this->logger = $logger ?: ObjectManager::getInstance()->get(\Psr\Log\LoggerInterface::class);
     }
 
     /**
-     * Retrieve assoc array of checkout configuration
-     *
+     * Retrieve assoc array of checkout configuration. Because it stores the
+     * variables in the HTML, nothing that is considered sensitive should be 
+     * returned from this method, otherwise it will be available publicly. The
+     * values can be found on the checkout page at:
+     * window.checkoutConfig.payment['komoju_payments']
      * @return array
      */
     public function getConfig() {
         $storeId = $this->session->getStoreId();
-        $isActive = $this->shouldEnableKomojuPayments();
+        $isActive = $this->config->isActive($storeId);
 
         return [
             'payment' => [
@@ -70,6 +77,15 @@ class ConfigProvider implements ConfigProviderInterface {
         ];
     }
 
+    /**
+     * Returns an array of available payment methods based on what the store
+     * owners have decided to allow for their store. The options list is being on
+     * the server instead of in the JS to ensure we can properly translate the
+     * options. If the store is set up to display text in Japanese we want to 
+     * make sure that the payments are also translated, which is not possible if
+     * the options are generated in the JS code.
+     * @return array
+     */
     private function createPaymentMethodOptions() {
         $paymentMethodOptions = [];
         if ($this->config->areCreditCardPaymentsEnabled()) {
@@ -81,10 +97,5 @@ class ConfigProvider implements ConfigProviderInterface {
         }
 
         return $paymentMethodOptions;
-    }
-
-    private function shouldEnableKomojuPayments() {
-        $storeId = $this->session->getStoreId();
-        return $this->config->isActive($storeId);
     }
 }
