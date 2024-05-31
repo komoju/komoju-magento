9082b3c033b36618a5137d2bc8fcc38762b746d8
Dinwy <whix2010@gmail.com>
2024-05-31 13:14:21 +0900

    Add doc block for restore quote from session class

--- src/app/code/Komoju/Payments/Observer/RestoreQuoteFromSession.php
+++ src/app/code/Komoju/Payments/Observer/RestoreQuoteFromSession.php
@@ -11,12 +11,21 @@ use Magento\Sales\Model\Order;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Psr\Log\LoggerInterface;
 
+/**
+ * Observer for restoring a customer's quote during the checkout process when the last order is in a pending payment state.
+ *
+ * This class is configured to listen to the `controller_action_postdispatch_checkout_index_index` event.
+ * It checks if the last real order placed within the session is still pending payment.
+ * If so, it restores the quote to the session.
+ * This action allows customers to return to their cart and potentially complete the transaction without needing to re-enter their information.
+ * For more information, please check the events_and_observers.md file in docs folder.
+ */
 class RestoreQuoteFromSession implements ObserverInterface
 {
-    protected $checkoutSession;
-    protected $sessionManager;
-    protected $quoteRepository;
-    private $logger;
+    protected CheckoutSession $checkoutSession;
+    protected SessionManager $sessionManager;
+    protected CartRepositoryInterface $quoteRepository;
+    private LoggerInterface $logger;
 
     public function __construct(
         CheckoutSession $checkoutSession,
@@ -37,12 +46,8 @@ class RestoreQuoteFromSession implements ObserverInterface
         if ($quote) {
             $order = $this->checkoutSession->getLastRealOrder();
 
-            if ($order) {
-                $orderStatus = $order->getStatus();
-
-                if ($orderStatus == ORDER::STATE_PENDING_PAYMENT) {
-                    $this->checkoutSession->restoreQuote();
-                }
+            if ($order && $order->getStatus() == Order::STATE_PENDING_PAYMENT) {
+                $this->checkoutSession->restoreQuote();
             }
         }
     }
